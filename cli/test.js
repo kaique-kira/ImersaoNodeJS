91950278acdece06b9b27f66b65963ca6a13332c
Kaique D Felipe <83593559+kaique-kira@users.noreply.github.com>
2023-07-18 18:04:39 -0300

    feat: criando opcoes de CLI e atualização de dados

--- cli/test.js
+++ cli/test.js
@@ -1,34 +1,51 @@
 const { deepEqual, ok } = require('assert');
-const Database = require('./database');
 const database = require('./database');
 const { REFUSED } = require('dns');
 
-const DEFAULT_ITEM_CADASTRAR = [
-    {
-        nome: 'Flash',
-        poder: 'Speed',
-        id: 1,
-    },
-];
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1,
+};
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: 'Batman',
+    poder: 'Dinheiro',
+    id: 2,
+};
 
 describe('Suite de manipulacão de Herois', () => {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
     });
     it('Deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR;
-        const [resultado] = await Database.listar(expected.id);
+        const [resultado] = await database.listar(expected.id);
         ok(resultado, expected);
     });
     it('Deve cadastrar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR;
-        const resultado = await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
-        const [actual] = await Database.listar(DEFAULT_ITEM_CADASTRAR.id);
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
         deepEqual([actual], expected);
     });
     it('Deve remover o heroi por id', async () => {
         const expected = true;
-        const resultado = await Database.remover(DEFAULT_ITEM_CADASTRAR.id);
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
         deepEqual(resultado, expected);
+    });
+    it('Deve atualizar um heroi pelo id', async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Robin',
+            poder: 'piruleta',
+        };
+        const novoDado = {
+            nome: 'Robin',
+            poder: 'piruleta',
+        };
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
+        deepEqual([resultado], expected);
     });
 });
